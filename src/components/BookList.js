30c7e13c64cb7641ff297ca870ab74e9e73765a4
Silas Mokone <Silas.Mokone@vodacom.co.za>
2022-02-24 12:36:47 +0200

    2nd commit

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { BookListSyled } from "./styles/BookList.styled";
+import MaterialTable from "material-table";
 
 export const BookList = () => {
+  const data = [
+    { book: "Atomic Habits", author: "James Clear" },
+    { book: "7 Habits ", author: "David Charles" },
+    { book: "Dinoneng", author: "Modibe Manepe" },
+    { book: "Bakgala ba tswang", author: "Janury Charles" },
+  ];
+
+  const columns = [
+    {
+      title: "Book",
+      field: "book",
+    },
+    {
+      title: "Author",
+      field: "author",
+    },
+  ];
   return (
-    <>
-      <h3>Books Cataloque</h3>
-      <ul className="list">
-        <li>
-          Book1 <span>-$400</span>
-          <button class="delete-btn">x</button>
-        </li>
-      </ul>
-    </>
+    <MaterialTable title="Books Cataloque" data={data} columns={columns} />
   );
 };
